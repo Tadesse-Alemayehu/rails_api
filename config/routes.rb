5eb78e6bd14fc5967ad7eee319b5af77e6b0094e
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-11 15:55:38 +0300

    Update: route from resource to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resource :users
+  resources :users, only: [:index, :show] do
+    resources :posts do
+      resources :likes
+      resources :comments
+    end
+  end
   # Defines the root path route ("/")
-  root "users#index"
+  # root "users#index"
 end
